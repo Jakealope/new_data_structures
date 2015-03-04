6fb7ef0f0f52670a699f70c2bac9f410a20884c6
Alicija Bulota <aabulota@smcm.edu>
2015-03-04 13:21:41 -0800

    instantiate a BSTNode

---
+++ bst.py
@@ -0,0 +1,6 @@
+class BSTNode(object):
+    def __init__(self, value, parent=None, left_child=None, right_child=None):
+        self.value = value
+        self.parent = parent
+        self.left = left_child
+        self.right = right_child
