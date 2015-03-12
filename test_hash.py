d3e9c560d2c58a918d490be802f93eb10af51e42
Jakealope <jake.anderson486@gmail.com>
2015-03-12 16:22:25 -0700

    wrote tests for hash, passing

---
+++ test_hash.py
@@ -0,0 +1,34 @@
+from hash_table import HashTable
+import pytest
+
+
+@pytest.fixture(scope='function')
+def _small():
+    h = HashTable()
+    h.set('batman', 'batman')
+    h.set('robin', 'robin')
+    return h
+
+
+def test_one(_small):
+    x = _small
+    assert x.get('batman') == 'batman'
+    assert x.get('batman') != 'robin'
+
+
+def test_dict():
+    x = HashTable()
+    with open("/usr/share/dict/words", "r") as afile:
+        for word in afile:
+            word.strip()
+            if not word:
+                break
+            x.set(word, word)
+        assert x.get(word) == word
+
+
+def test_ordval():
+    x = HashTable()
+    x.set('this is a really big sentence to break it',
+          'this is a really big sentence to break it')
+    assert x.get('this is a really big sentence to break it') == 'this is a really big sentence to break it'
