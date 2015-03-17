6af37ef9bd280e302d8241b1cb33b6df91409fa7
Jakealope <jake.anderson486@gmail.com>
2015-03-16 18:42:28 -0700

    added timeit stuff

--- insert_sort.py
+++ insert_sort.py
@@ -1,3 +1,5 @@
+import timeit
+
 
 def sorter(self):
     for x in range(1, len(self)):
@@ -10,8 +12,31 @@ def sorter(self):
             position = position - 1
 
         self[position] = currentval
+    return self
 
 if __name__ == '__main__':
-    xlist = [12, 45, 23, 98, 54, 234]
-    sorter(xlist)
-    print xlist
+
+    def easy_sort():
+        x = range(100)
+        sorter(x)
+
+    def hard_sort():
+        x = range(100, 0, -1)
+        sorter(x)
+
+    def long_sort():
+        x = range(1000)
+        sorter(x)
+
+    def long_hard_sort():
+        x = range(1000, 0, -1)
+        sorter(x)
+
+    print "This is sorting a range from 1-100"
+    print(timeit.Timer("easy_sort()", setup="from __main__ import easy_sort").timeit(number=100))
+    print "This is sorting a range 100-1"
+    print(timeit.Timer("hard_sort()", setup="from __main__ import hard_sort").timeit(number=100))
+    print "This is sorting a range from 1-1000"
+    print(timeit.Timer("long_sort()", setup="from __main__ import long_sort").timeit(number=100))
+    print "This is sorting a range 1000-1"
+    print(timeit.Timer("long_hard_sort()", setup="from __main__ import long_hard_sort").timeit(number=100))
