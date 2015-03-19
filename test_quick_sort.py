192f82046f621df4dc534863a3034562c1d12331
Jakealope <jake.anderson486@gmail.com>
2015-03-18 19:50:48 -0700

    finished tests

--- test_quick_sort.py
+++ test_quick_sort.py
@@ -1,27 +1,27 @@
-from quick_sort import Q_sort
+from quick_sort import q_sort
 import random
 
 
 def test_sort_one():
     x = range(10)
-    assert Q_sort(x) == x
+    assert q_sort(x) == x
 
 
 def test_sort_two():
-    x = range(10, 1, -1)
-    assert Q_sort(x) == range(10)
+    x = range(10, 0, -1)
+    assert q_sort(x) == range(1, 11)
 
 
 def test_sort_big():
-    x = range(1000)
-    assert Q_sort(x) == x
+    x = range(100)
+    assert q_sort(x) == x
 
 
 def test_sort_big_backwards():
-    x = range(1000, 1, -1)
-    assert Q_sort(x) == x
+    x = range(100, 0, -1)
+    assert q_sort(x) == range(1, 101)
 
 
 def test_sort_random():
     x = random.sample(range(100), 100)
-    assert Q_sort(x) == range(100)
+    assert q_sort(x) == range(100)
