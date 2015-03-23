4910379a0e893b3cd2980a30afff9b14efc7571d
Jakealope <jake.anderson486@gmail.com>
2015-03-22 17:29:16 -0700

    added string sorting, passing

--- radix_sort.py
+++ radix_sort.py
@@ -1,22 +1,86 @@
+import timeit
+import random
 
 
-def sort_radix(alist):
-    length = len(alist)
-    total = 10
-    bucket = 1
-    while True:
-        new_list = [[] for i in range(total)]
-        for value in alist:
-            smallest_val = value % total
-            smallest_val /= bucket
-            new_list[smallest_val].append(value)
-        total = total * 10
-        bucket = bucket * 10
-    if len(new_list[0]) == length:
-        return new_list[0]
+def r_sort(alist, base=10):
 
-    rand_list = []
-    rand_list_append = rand_list.append
-    for x in new_list:
-        for y in x:
-            rand_list_append(y)
+    def list_to_buckets(alist, base, iteration):
+        buckets = [[] for i in range(base)]
+        for number in alist:
+            digit = (number // (base ** iteration)) % base
+            buckets[digit].append(number)
+        return buckets
+
+    def buckets_to_list(buckets):
+        alist = []
+        for bucket in buckets:
+            alist.extend(bucket)
+        return alist
+
+    max_val = 0
+    for i in alist:
+        if i > max_val:
+            max_val = i
+
+    it = 0
+
+    while ((base ** it) <= max_val):
+        alist = buckets_to_list(list_to_buckets(alist, base, it))
+        it += 1
+
+    return alist
+
+
+def string_radix(arr, base=128):
+    try:
+        longest = find_longest_string(arr)
+    except IndexError:
+        return arr
+
+    index_num = 0
+    index_place = (len(longest) - 1)
+    buckets = [[] for x in range(base)]
+
+    while index_place >= 0:
+        for val in arr:
+            try:
+                index_num = ord(val[index_place])
+            except IndexError:
+                index_num = 0
+            buckets[index_num].append(val)
+
+        transfer_list = []
+        for bucket in buckets:
+            for i in range(len(bucket)):
+                transfer_list.append(bucket.pop(0))
+        arr = transfer_list
+        index_place -= 1
+    return arr
+
+
+def find_longest_string(arr):
+    longest = arr[0]
+    for val in arr:
+        if len(val) > len(longest):
+            longest = val
+    return longest
+
+
+if __name__ == '__main__':
+
+    def easy_sort():
+        x = range(1000)
+        r_sort(x)
+
+    def hard_sort():
+        x = random.sample(range(1000), 1000)
+        r_sort(x)
+
+    print "This is sorting a range of 1000"
+    print(timeit.Timer("easy_sort()",
+          setup="from __main__ import easy_sort")
+          .timeit(number=1000))
+    print "This is sorting a random range 1-1000"
+    print(timeit.Timer("hard_sort()",
+          setup="from __main__ import hard_sort")
+          .timeit(number=1000))
