398caf7582274370511efe6bb683b3cd79b9ff6b
Jakealope <jake.anderson486@gmail.com>
2015-04-14 14:32:18 -0700

    added parenthisis to print for python3 and changed if statement to try/except for pythonesque reasons

--- quick_sort.py
+++ quick_sort.py
@@ -2,16 +2,14 @@ import timeit
 import random
 
 
-def q_sort(self):
+def q_sort(alist):
     lower = []
     p_list = []
     higher = []
-    if len(self) <= 1:
-        return self
 
-    else:
-        pivot = random.choice(self)
-        for i in self:
+    try:
+        pivot = random.choice(alist)
+        for i in alist:
             if i < pivot:
                 lower.append(i)
             elif i > pivot:
@@ -21,20 +19,22 @@ def q_sort(self):
         lower = q_sort(lower)
         higher = q_sort(higher)
         return lower + p_list + higher
-
+    except:
+            if len(alist) <= 1:
+                return alist
 if __name__ == '__main__':
 
-    def easy_sort():
+    def hard_sort():
         x = random.sample(range(100), 100)
         q_sort(x)
 
-    def hard_sort():
+    def easy_sort():
         x = range(100)
         q_sort(x)
 
-    print "This is sorting a random range of 100"
+    print("This is sorting a random range of 100")
     print(timeit.Timer("easy_sort()",
                        setup="from __main__ import easy_sort").timeit(number=1000))
-    print "This is sorting a range 1-100"
+    print("This is sorting a range 1-100")
     print(timeit.Timer("hard_sort()",
                        setup="from __main__ import hard_sort").timeit(number=1000))
